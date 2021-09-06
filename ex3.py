d99c2b3916909bad949b6672d7209f3abbbcf4de
ppedraum <pedrovirtuozo@gmail.com>
2021-09-06 16:51:07 -0300

    Initial commit

---
+++ ex3.py
@@ -0,0 +1,3 @@
+#3
+numeros = [1, 2, 3, 4, 5, 6]
+print("Soma dos números: ", sum(numeros))
---
+++ ex9.py
@@ -0,0 +1,4 @@
+#9
+numeros = [1, 5, 3, 8, 7, 14, 1314, 13635, 17654, 2, 7]
+numeros.sort()
+print(numeros)
